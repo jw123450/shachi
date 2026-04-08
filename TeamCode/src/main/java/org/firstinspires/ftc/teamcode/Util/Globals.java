f3e953ef04c31af0cb284daaa2cfd12524cee21c
jw123450 <156040007+jw123450@users.noreply.github.com>
2026-04-07 19:22:31 -1000

    tune goal poses
    old poses saved in comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Globals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Util/Globals.java
@@ -6,9 +6,9 @@ import com.pedropathing.geometry.Pose;
 @Config
 public class Globals {
     public static Pose autoEndPose = new Pose(72,7.81, Math.toRadians(90)); // default for easier testing
-    public static double blueGoalX = 10.5; // TODO: tune locations depending preferred backboard aim
-    public static double blueGoalY = 139; // TODO
-    public static double redGoalX = 134.5; // TODO
-    public static double redGoalY = 139; // TODO
+    public static double blueGoalX = 15.5; // TODO: tune locations depending preferred backboard aim //old: 10.5
+    public static double blueGoalY = 136; // TODO //old: 139
+    public static double redGoalX = 140; // TODO //old: 134.5
+    public static double redGoalY = 136; // TODO //old: 139
     public static boolean blueAlliance = true;
 }
